f2999dbd29dd5bf7a51cbbb0ac40a755ff0f0978
jonghkim <quantic.jh@gmail.com>
2019-12-15 01:38:40 +0900

    position fix

--- static/mods/draw_small_scale.js
+++ static/mods/draw_small_scale.js
@@ -26,7 +26,7 @@ tilde.updateNeedle = function() {
 			.append('svg')
 			.attr('width',function(d,i){
 				var x = +d3.select("#needle_annotation")[0][0].offsetWidth
-					return x*2
+					return x
 			})
 			.attr('height',function(d,i){
 				var x = +d3.select("#needle_annotation")[0][0].offsetWidth/4 + tilde.dropheight
